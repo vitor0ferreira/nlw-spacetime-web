2335189bdd8da73144d82c5bd6a98290431e6687
vitor0ferreira <vitor_hugo.f.s@hotmail.com>
2023-05-16 21:23:43 -0300

    fonts setting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 p-6" >
-      <h1 className="text-4xl font-bold">Sua Cápsula do Tempo</h1>
+      <h1 className="text-4xl font-alt">Sua Cápsula do Tempo</h1>
     </div>
   )
 }
